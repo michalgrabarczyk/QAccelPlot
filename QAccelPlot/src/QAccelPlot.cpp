81aaa6d366bd9feaedb10dff27a613df53d36962
Michal Grabarczyk <michalgrabarczyk@hotmail.com>
2026-09-13 22:05:16 +0200

    Relayout axes when tick overlap or label overflow inputs change
    layoutAxes() positions and sizes each axis widget using
    inwardTickOverlap() (max of tickLengthIn/subtickLengthIn, minus
    axisLinePadding) and labelOverflow() (depends on orientation), but
    connectAxisSignals() only triggered a relayout on visibleChanged and
    layoutSizeChanged. Changing axisLinePadding, orientation, or the
    ticker's tickLengthIn/subtickLengthIn at runtime left the axis
    overlapping the plot area until an unrelated relayout happened to run.
    
    Connect axisLinePaddingChanged, orientationChanged, and the ticker's
    tickLengthInChanged/subtickLengthInChanged to layoutAxes().

--- QAccelPlot/src/QAccelPlot.cpp
+++ QAccelPlot/src/QAccelPlot.cpp
@@ -608,10 +608,12 @@ void QAccelPlot::connectAxisSignals(Axis* axis)
     connect(axis, &QQuickItem::visibleChanged, this, &QAccelPlot::layoutAxes, Qt::UniqueConnection);
     connect(axis, &Axis::layoutSizeChanged, this, &QAccelPlot::layoutAxes, Qt::UniqueConnection);
     connect(axis, &QObject::destroyed, this, &QAccelPlot::axisDestroyed, Qt::UniqueConnection);
-    // Grid line positions are derived from tickCount/subtickCount (see GridNode::update()),
-    // so the plot must repaint when either changes, not just the axis widget itself.
     connect(axis->ticker(), &AxisTicker::tickCountChanged, this, &QAccelPlot::update, Qt::UniqueConnection);
     connect(axis->ticker(), &AxisTicker::subtickCountChanged, this, &QAccelPlot::update, Qt::UniqueConnection);
+    connect(axis, &Axis::axisLinePaddingChanged, this, &QAccelPlot::layoutAxes, Qt::UniqueConnection);
+    connect(axis, &Axis::orientationChanged, this, &QAccelPlot::layoutAxes, Qt::UniqueConnection);
+    connect(axis->ticker(), &AxisTicker::tickLengthInChanged, this, &QAccelPlot::layoutAxes, Qt::UniqueConnection);
+    connect(axis->ticker(), &AxisTicker::subtickLengthInChanged, this, &QAccelPlot::layoutAxes, Qt::UniqueConnection);
 }
 
 void QAccelPlot::disconnectAxisSignals(Axis* axis)
@@ -627,6 +629,10 @@ void QAccelPlot::disconnectAxisSignals(Axis* axis)
     disconnect(axis, &QObject::destroyed, this, &QAccelPlot::axisDestroyed);
     disconnect(axis->ticker(), &AxisTicker::tickCountChanged, this, &QAccelPlot::update);
     disconnect(axis->ticker(), &AxisTicker::subtickCountChanged, this, &QAccelPlot::update);
+    disconnect(axis, &Axis::axisLinePaddingChanged, this, &QAccelPlot::layoutAxes);
+    disconnect(axis, &Axis::orientationChanged, this, &QAccelPlot::layoutAxes);
+    disconnect(axis->ticker(), &AxisTicker::tickLengthInChanged, this, &QAccelPlot::layoutAxes);
+    disconnect(axis->ticker(), &AxisTicker::subtickLengthInChanged, this, &QAccelPlot::layoutAxes);
 }
 
 void QAccelPlot::axisDestroyed(QObject* object)
