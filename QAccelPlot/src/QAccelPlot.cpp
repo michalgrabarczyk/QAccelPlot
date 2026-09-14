2907822f9d3c1b9957559cc3dee98304ea9c01e6
Michal Grabarczyk <michalgrabarczyk@hotmail.com>
2026-09-14 08:08:09 +0200

    Remove low-value comment on grid refresh connections

--- QAccelPlot/src/QAccelPlot.cpp
+++ QAccelPlot/src/QAccelPlot.cpp
@@ -608,8 +608,6 @@ void QAccelPlot::connectAxisSignals(Axis* axis)
     connect(axis, &QQuickItem::visibleChanged, this, &QAccelPlot::layoutAxes, Qt::UniqueConnection);
     connect(axis, &Axis::layoutSizeChanged, this, &QAccelPlot::layoutAxes, Qt::UniqueConnection);
     connect(axis, &QObject::destroyed, this, &QAccelPlot::axisDestroyed, Qt::UniqueConnection);
-    // Grid line positions are derived from tickCount/subtickCount (see GridNode::update()),
-    // so the plot must repaint when either changes, not just the axis widget itself.
     connect(axis->ticker(), &AxisTicker::tickCountChanged, this, &QAccelPlot::update, Qt::UniqueConnection);
     connect(axis->ticker(), &AxisTicker::subtickCountChanged, this, &QAccelPlot::update, Qt::UniqueConnection);
 }
