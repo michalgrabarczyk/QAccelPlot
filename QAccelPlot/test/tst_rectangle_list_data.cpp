a8e3487f17370e08833e5a572d4f6bb0c852a33f
Michal Grabarczyk <michalgrabarczyk@hotmail.com>
2026-09-17 17:07:07 +0200

    Refresh rectangle coordinates when axis scale changes

--- QAccelPlot/test/tst_rectangle_list_data.cpp
+++ QAccelPlot/test/tst_rectangle_list_data.cpp
@@ -6,13 +6,23 @@
 // See COMMERCIAL-LICENSING.md for contact information.
 //
 #include "QAccelPlot/axis/Axis.hpp"
+#include "QAccelPlot/materials/RectMaterial.hpp"
 #include "QAccelPlot/shapes/RectangleList.hpp"
 
+#include <QQuickWindow>
+#include <QSGGeometryNode>
 #include <QtTest/QtTest>
 
 #include <array>
 
 namespace QAccelPlot {
+
+namespace {
+class RenderableRectangles final : public RectangleList {
+public:
+    using RectangleList::updatePaintNode;
+};
+}
 
 class RectangleListDataTest : public QObject {
     Q_OBJECT
@@ -22,6 +32,8 @@ private slots:
     void invalidRawArgumentsAreRejected();
     void rawDoubleDataPreservesModernEpochPrecision();
     void variantListDataPreservesModernEpochPrecision();
+    void scaleChangesRefreshRenderCoordinates_data();
+    void scaleChangesRefreshRenderCoordinates();
 };
 
 void RectangleListDataTest::hoverEnvironmentControlsAcceptance()
@@ -103,6 +115,50 @@ void RectangleListDataTest::variantListDataPreservesModernEpochPrecision()
 
     QCOMPARE(xAxis.dataMin(), epochMilliseconds);
     QCOMPARE(xAxis.dataMax(), epochMilliseconds + 1.0);
+}
+
+void RectangleListDataTest::scaleChangesRefreshRenderCoordinates_data()
+{
+    QTest::addColumn<bool>("horizontal");
+    QTest::addColumn<bool>("logScale");
+    QTest::newRow("x-linear-to-log") << true << true;
+    QTest::newRow("x-log-to-linear") << true << false;
+    QTest::newRow("y-linear-to-log") << false << true;
+    QTest::newRow("y-log-to-linear") << false << false;
+}
+
+void RectangleListDataTest::scaleChangesRefreshRenderCoordinates()
+{
+    QFETCH(bool, horizontal);
+    QFETCH(bool, logScale);
+    auto xAxis = Axis{};
+    auto yAxis = Axis{};
+    xAxis.setViewportMin(1);
+    xAxis.setViewportMax(1000);
+    yAxis.setViewportMin(1);
+    yAxis.setViewportMax(1000);
+    auto& changedAxis = horizontal ? xAxis : yAxis;
+    changedAxis.setLogScale(!logScale);
+    auto window = QQuickWindow{};
+    auto rectangles = RenderableRectangles{};
+    rectangles.setParentItem(window.contentItem());
+    rectangles.setXAxis(&xAxis);
+    rectangles.setYAxis(&yAxis);
+    rectangles.setPlotRect({0, 0, 100, 100});
+    const auto data = std::array<double, 4>{10, 20, 100, 200};
+    rectangles.setData(data.data(), 1);
+    auto node = std::unique_ptr<QSGNode>{rectangles.updatePaintNode(nullptr, nullptr)};
+    QVERIFY(node);
+
+    changedAxis.setLogScale(logScale);
+    node.reset(rectangles.updatePaintNode(node.release(), nullptr));
+
+    auto* geometry = static_cast<QSGGeometryNode*>(node.get());
+    const auto* material = static_cast<RectMaterial*>(geometry->material());
+    const auto expectedMin = QVector2D{float(1 - (xAxis.logScale() ? 0 : 10)), float(1 - (yAxis.logScale() ? 0 : 20))};
+    QCOMPARE(material->domainMin, expectedMin);
+    QCOMPARE(material->logScaleX, xAxis.logScale() ? 1.0f : 0.0f);
+    QCOMPARE(material->logScaleY, yAxis.logScale() ? 1.0f : 0.0f);
 }
 
 } // namespace QAccelPlot
